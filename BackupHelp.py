e1a2422b9e0ea5b4e97d541663d1195b36319808
Rasmus Karell <rk@rasmuskarell.com>
2021-05-13 21:37:54 +0300

    Ignore .jpg files with different mod time

--- BackupHelp.py
+++ BackupHelp.py
@@ -24,8 +24,12 @@ def CompareDirectories(path1, path2, ignorePaths):
         for item2 in path2.iterdir():
             if item1.name == item2.name:
                 found = True
+                
+                if item1.suffix == ".jpg":          # I rarely change .jpg files but they might still have different modified times for some reason -> ignore them
+                    break
+
                 if item1.is_dir():
-                    CompareDirectories(item1,item2)
+                    CompareDirectories(item1, item2, ignorePaths)
                 else:
                     time1 = datetime.datetime.fromtimestamp(item1.stat().st_mtime)
                     time2 = datetime.datetime.fromtimestamp(item2.stat().st_mtime)
@@ -33,6 +37,8 @@ def CompareDirectories(path1, path2, ignorePaths):
                     # A little ugly solution here, but sometimes the last modified time differs 1 second or about one hour even if they were modified at the same time. The latter is probably related to the summer time and different memory technologies. Let's ignore those.
                     if difference > datetime.timedelta(0,2) and not (difference > datetime.timedelta(0,58,0,0,59) and difference <= datetime.timedelta(0,0,0,0,0,1)):
                         print("diff mod time\t" + str(item1) + " | " + str(difference))
+
+                break
                         
         if found == False:
             print("not found\t" + str(item1))
